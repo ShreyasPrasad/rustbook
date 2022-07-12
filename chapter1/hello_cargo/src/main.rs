f3a34b68e72f415eb2c5364f26113bbfd2e26071
unknown <missonpkmn@gmail.com>
2022-07-12 01:04:01 -0400

    Completed chapter 1

---
+++ chapter1/hello_cargo/src/main.rs
@@ -0,0 +1,13 @@
+// Cargo expects source files to live in src directory
+
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+Key commands are:
+    cargo build (builds, should be used for development)
+    cargo run (builds if necessary, and runs executable)
+    cargo check (makes sure that code compiles, but no executable produced)
+    cargo build --release (optimized build, should be used for production)
+*/
---
+++ chapter1/hello_world/hello_world.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!"); // most lines end with a semicolon
+}
+
+// rustc is similar to gcc or clang
+// Rust is an ahead-of-time compiled language
